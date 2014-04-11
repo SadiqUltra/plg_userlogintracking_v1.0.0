99bc34a4bc07322345b7845895bd3581a0f75085
SadiqUltra <sadikultra@gmail.com>
2014-04-11 21:50:42 +0600

    Create install.mysql.utf8.sql

---
+++ sql/install.mysql.utf8.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `#__userlogin_tracking`;
+
+CREATE TABLE IF NOT EXISTS `#__userlogin_tracking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
